c6ffdee482d4f5a050ed2a8fbf04d42311c1ee1e
Amaar Quadri <amaarquadri@gmail.com>
2020-04-01 18:35:40 -0400

    change default name

--- src/FileIO.py
+++ src/FileIO.py
@@ -23,14 +23,14 @@ def load_star(file_name=None):
 
 def save_stellar_data(stellar_data, file_name=None):
     if file_name is None:
-        file_name = 'star_pickle_' + get_timestamp()
+        file_name = 'stellar_data_pickle_' + get_timestamp()
     with open('../Stellar Data/' + file_name + '.pickle', 'wb') as pout:
         pickle.dump(stellar_data, pout)
 
 
 def load_stellar_data(file_name=None):
     if file_name is None:
-        file_name = 'star_pickle_' + get_timestamp()
+        file_name = 'stellar_data_pickle_' + get_timestamp()
     with open('../Stellar Data/' + file_name + '.pickle', 'rb') as pin:
         stellar_data = pickle.load(pin)
     return stellar_data
