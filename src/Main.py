9f9b85b023a7e826d2bc9f6401a4992ad16691bf
Amaar Quadri <amaarquadri@gmail.com>
2020-04-09 23:42:54 -0400

    Documentation (#7)
    * added dataset of standard stars
    
    * changed file
    
    * documentation
    
    * Added testing function
    
    * added sequence graphing
    
    * removed data points below 4 million K
    
    * removed outliers, fixed typo
    
    * switch to StellarConfiguration class
    central density predictions test
    
    * added toExcel function, added break when max tolerance in python reached, removed deprecated code

--- src/Main.py
+++ src/Main.py
@@ -1,14 +1,17 @@
 import numpy as np
-from NumericalIntegration import solve_numerically
-from Units import R_sun, million_K
-from Graphing import graph_star
-from HR_Plot import get_luminosity, hr_plot, generate_hr
-from ExampleStar import load_example_data
+from src.StellarStructureEquations import StellarConfiguration
+from src.NumericalIntegration import solve_bvp
+from src.Units import R_sun, million_K, g, cm
+from src.Graphing import graph_star
+from src.ExampleStar import load_example_data
+from src.StarSequenceGenerator import generate_stars
 
 
 if __name__ == '__main__':
-    reference_data = load_example_data()
-    # r_values, state_values, error = trial_solution_rk45(58.56399787e3, 8.23544e+06, R_sun / 10 ** 5, return_star=True)
-    # graph_star(r_values, state_values, "Test_hardcode_rk45", reference_data=load_example_data)
-
-    generate_hr()
+    reference_data = load_example_data(file_name='../Example Stars/highmass_star.txt')
+    config = StellarConfiguration()
+    error, r_values, state_values = solve_bvp(24 * million_K,
+                                              rho_c_guess=100 * g / cm ** 3,
+                                              config=config)
+    print('Error', error)
+    graph_star(r_values, state_values, 'high_mass2', config=config)
---
+++ src/StellarSequenceGraphing.py
@@ -0,0 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.FileIO import get_timestamp, load_stellar_data
+from src.StarSequenceGenerator import T_surface_index, L_surface_index, M_surface_index, R_index
+from src.Units import L_sun, K, M_sun, R_sun
+
+
+def plot_sequence(stellar_data_lists, file_name=None):
+    if file_name is None:
+        file_name = 'test' + get_timestamp()
+
+    # Remove stars that didnt converge
+    stellar_data_lists = [(stellar_data[:, stellar_data[M_surface_index, :] < 1000 * M_sun], config)
+                          for stellar_data, config in stellar_data_lists]
+
+    plot_HR(stellar_data_lists, file_name + '_HR')
+    plot_LM(stellar_data_lists, file_name + '_LM')
+    plot_RM(stellar_data_lists, file_name + '_RM')
+
+
+def plot_HR(stellar_data_lists, file_name=None):
+    if file_name is None:
+        file_name = 'HR_plot_' + get_timestamp()
+
+    for stellar_data, config in stellar_data_lists:
+        label = config.describe_gravity_modifications() if config.has_gravity_modifications() else 'standard'
+        plt.scatter(np.log10(stellar_data[T_surface_index, :] / K),
+                    np.log10(stellar_data[L_surface_index, :] / L_sun), label=label)
+    if len(stellar_data_lists) > 1:
+        plt.legend()
+    plt.title('HR Diagram')
+    plt.xlabel('$Log_{10}$ of Temperature ($Log_{10}$ (K))')
+    plt.ylabel(r'$Log_{10}$ of Luminosity ($Log_{10}$ ($L_{\odot}$))')
+    plt.gca().invert_xaxis()
+    plt.savefig('../Graphs/' + file_name + '.png')
+    plt.clf()
+
+
+def plot_LM(stellar_data_lists, file_name=None):
+    if file_name is None:
+        file_name = 'LM_plot_' + get_timestamp()
+
+    for stellar_data, config in stellar_data_lists:
+        label = config.describe_gravity_modifications() if config.has_gravity_modifications() else 'standard'
+        plt.scatter(np.log10(stellar_data[M_surface_index, :] / M_sun),
+                    np.log10(stellar_data[L_surface_index, :] / L_sun), label=label)
+        line = np.polyfit(np.log10(stellar_data[M_surface_index, :] / M_sun),
+                          np.log10(stellar_data[L_surface_index, :] / L_sun), deg=1)
+        print('LM Line of best fit for', label, ': y=', line[0], '* x +', line[1])
+    if len(stellar_data_lists) > 1:
+        plt.legend()
+    plt.title('LM Diagram')
+    plt.xlabel(r'$Log_{10}$ of Mass ($Log_{10}$ ($M_{\odot}$))')
+    plt.ylabel(r'$Log_{10}$ of Luminosity ($Log_{10}$ ($L_{\odot}$))')
+    plt.savefig('../Graphs/' + file_name + '.png')
+    plt.clf()
+
+
+def plot_RM(stellar_data_lists, file_name=None):
+    if file_name is None:
+        file_name = 'RM_plot_' + get_timestamp()
+
+    for stellar_data, config in stellar_data_lists:
+        label = config.describe_gravity_modifications() if config.has_gravity_modifications() else 'standard'
+        plt.scatter(np.log10(stellar_data[M_surface_index, :] / M_sun),
+                    np.log10(stellar_data[R_index, :] / R_sun), label=label)
+        line = np.polyfit(np.log10(stellar_data[M_surface_index, :] / M_sun),
+                          np.log10(stellar_data[R_index, :] / L_sun), deg=1)
+        print('RM Line of best fit for', label, ': y=', line[0], '* x +', line[1])
+    if len(stellar_data_lists) > 1:
+        plt.legend()
+    plt.title('RM Diagram')
+    plt.xlabel(r'$Log_{10}$ of Mass ($Log_{10}$ ($M_{\odot}$))')
+    plt.ylabel(r'$Log_{10}$ of Radius ($Log_{10}$ ($R_{\odot}$))')
+    plt.savefig('../Graphs/' + file_name + '.png')
+    plt.clf()
+
+
+if __name__ == '__main__':
+    file_names = ['standard_stellar_data']
+    dataset = [load_stellar_data(file_name=file_name, return_config=True) for file_name in file_names]
+    plot_sequence(dataset)
